bb487562f7d5ae9b39899757cde7f9c43a1bdb06
Andrew Kim <h.andrew.kim@gmail.com>
2014-03-06 16:15:05 -0600

    Display bundles in tab

--- web/src/main/resources/js/core.js
+++ web/src/main/resources/js/core.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
   // load apps into menu
   $.getJSON('/web.json', function(apps) {
     console.log(apps);
-    var $li = $(document.createElement('li'));
-    var $a = $(document.createElement('a'));
-    $a.append('test run').attr('href', '#');
-    $li.append($a);
+    $.each(apps, function(key, app) {
+      var $li = $(document.createElement('li'));
+      var $a = $(document.createElement('a'));
+      $a.append(app.name).attr('href', '#');
+      $li.append($a);
 
-    $('#menu ul').append($li);
+      $('#menu ul').append($li);
+    });
   });
 });
