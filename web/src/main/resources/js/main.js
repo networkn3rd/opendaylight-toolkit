4bcd138dd835dfbc7c8bca964263eeaa37903666
Andrew Kim <h.andrew.kim@gmail.com>
2014-03-08 18:33:22 -0600

    Add frontend libraries
    Bower
    
    JS:
    jQuery
    RequireJS
    BackboneJS
    UnderscoreJS
    
    CSS:
    Pure

--- web/src/main/resources/js/main.js
+++ web/src/main/resources/js/main.js
@@ -1,3 +1,14 @@
-jQuery(document).ready(function($) {
-	$('#canvas').html("JS Placeholder")
+// Filename: main.js
+
+require.config({
+  paths: {
+    "jquery": "/js/ext/jquery/dist/jquery.min",
+    "underscore": "/js/ext/underscore/underscore",
+    "backbone": "/js/ext/backbone/backbone"
+  }
+});
+require([
+  'app'
+], function(App) {
+  new App.initialize();
 });
