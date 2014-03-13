0c24f00ba54c1885a5c42a9abb0ac2b77b390a29
violinlakshmi <violinlakshmi@gmail.com>
2014-03-13 11:24:00 -0700

    Display devices and device properties
    Display all the devices and onclick of a device id, show the devices
    properties.

--- web/src/main/resources/js/views/DeviceView.js
+++ web/src/main/resources/js/views/DeviceView.js
@@ -5,25 +5,37 @@ define(
         'underscore',
         'collections/DevicesCollection',
         'models/DeviceModel',
+        'views/DevicePropertiesView',
         'ext/text/text!templates/device.html'
-    ], function($, Backbone, _, DevicesCollection, DeviceModel, DeviceTemplate) {
+    ], function($, Backbone, _, DevicesCollection, DeviceModel, DevicePropertiesView, DeviceTemplate) {
 
     var DeviceView = Backbone.View.extend({
         el: $("#main"),
+        initialize: function() {
+            var self = this;
+            this.collection = new DevicesCollection();
+            this.collection.url = "/controller/nb/v2/switchmanager/default/nodes";
+            this.collection.fetch({
+                success: function(coll, response) {
+                    console.log("passed collection call to get devices", response);
+                    self.render();
+                }
+            });
+        },
         render: function() {
             console.log("DevicesView initialize called");
-            this.collection = new DevicesCollection();
-            this.deviceModel = new DeviceModel({
-                'name': 'device1',
-                'ipAddress': '1.2.3.4'
-            });
-            this.collection.add(this.deviceModel);
-            var templateVariables = {
-                'device_name': this.deviceModel.get('name'),
-                'device_ip': this.deviceModel.get('ipAddress')
-            };
-            var compiledTemplate = _.template(DeviceTemplate, templateVariables);
-            $(this.el).append(compiledTemplate);
+            var that = this;
+            var compiledTemplate = _.template(DeviceTemplate, {devices: that.collection.models[0].get('nodeProperties')});
+            $(this.el).append($(compiledTemplate).html());
+        },
+        events: {
+            "click tr": "getNodeProperties"
+        },
+        getNodeProperties: function(evt) {
+            var $tr = $(evt.currentTarget);
+            var devicePropsView = new DevicePropertiesView();
+            devicePropsView.nodeId = $tr.attr("data-nodeId");
+            devicePropsView.render();
         }
     });
     return DeviceView;
