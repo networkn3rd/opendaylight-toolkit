4ab52ef402cd7f83197771939574ec1b7ec38eed
Andrew Kim <h.andrew.kim@gmail.com>
2014-03-18 16:00:49 -0500

    Add flows archetype

--- main/archetypes/archetype-app-flow/src/main/resources/archetype-resources/src/main/resources/js/app.js
+++ main/archetypes/archetype-app-flow/src/main/resources/archetype-resources/src/main/resources/js/app.js
@@ -1,11 +1,12 @@
+#set( $symbol_dollar = '$' )
 // Filename: app.js
 
 define([
   // These are path alias that we configured in our bootstrap
-  'jquery',     // lib/jquery/jquery
-  'underscore', // lib/underscore/underscore
-  'backbone',    // lib/backbone/backbone
-  'views/FlowsListView'
+  'jquery',     				// lib/jquery/jquery
+  'underscore', 				// lib/underscore/underscore
+  'backbone',    				// lib/backbone/backbone
+  '/${artifactId}/js/views/FlowsListView.js'	// app
 ], function($, _, Backbone, FlowsListView){
   var initialize = function() {
     var flowsListView = new FlowsListView(); // this calls initialize which in turn calls render
