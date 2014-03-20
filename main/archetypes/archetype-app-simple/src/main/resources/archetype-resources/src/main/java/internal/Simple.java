ead35391c422b785e6ec84c71699380a69d129bb
Madhu Venugopal <mavenugo@gmail.com>
2014-03-19 19:12:48 -0700

    Modified the NB-API to return a List of SimpleData

--- main/archetypes/archetype-app-simple/src/main/resources/archetype-resources/src/main/java/internal/Simple.java
+++ main/archetypes/archetype-app-simple/src/main/resources/archetype-resources/src/main/java/internal/Simple.java
@@ -4,37 +4,41 @@ package ${package}.internal;
 
 import ${package}.ISimple;
 import ${package}.SimpleData;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.UUID;
 import java.util.Map;
+
 import org.opendaylight.controller.sal.utils.Status;
 import org.opendaylight.controller.sal.utils.StatusCode;
 
 public class Simple implements ISimple {
     private Map<UUID, SimpleData> data;
     protected static final Logger log = LoggerFactory.getLogger(Simple.class);
-	@Override
+    @Override
     public UUID createData(SimpleData datum) {
         UUID uuid = UUID.randomUUID();
-        data.put(uuid, datum);
+        SimpleData sData = new SimpleData(uuid.toString(), datum.getFoo(), datum.getBar());
+        data.put(uuid, sData);
         return uuid;
     }
-	@Override
+    @Override
     public SimpleData readData(UUID uuid) {
         return data.get(uuid);
     }
-	@Override
+    @Override
     public Map<UUID, SimpleData> readData() {
         return data;
     }
-	@Override
+    @Override
     public Status updateData(UUID uuid, SimpleData datum) {
         data.put(uuid, datum);
         return new Status(StatusCode.SUCCESS);
     }
-	@Override
+    @Override
     public Status deleteData(UUID uuid) {
         data.remove(uuid);
         return new Status(StatusCode.SUCCESS);
